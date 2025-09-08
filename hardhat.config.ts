4a9f8489ef9a416f481e61fa4eb4ea106da3469b
maxime <toyigbemaximed@gmail.com>
2025-09-08 14:07:28 +0200

    mise  en  place    du  config  hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,6 +22,7 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
+    
     hardhatMainnet: {
       type: "edr-simulated",
       chainType: "l1",
@@ -36,6 +37,28 @@ const config: HardhatUserConfig = {
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    hardhat: {
+      type: "edr-simulated", // Ajout du champ type requis
+      chainId: 31337, // Réseau local Hardhat
+      accounts: {
+        count: 10, // 10 comptes pour les tests
+        initialIndex: 0,
+        accountsBalance: "10000000000000000000000", // 10,000 ETH par compte
+      },
+      gas: "auto",
+      gasPrice: "auto",
+      blockGasLimit: 30000000, // Limite de gaz élevée
+      allowUnlimitedContractSize: true, // Pour les tests locaux
+      loggingEnabled: true, // Logs pour le débogage
+    },
+    localhost: {
+      type: "http", // Type HTTP pour le nœud local
+      url: "http://127.0.0.1:8545", // URL par défaut du nœud Hardhat
+      chainId: 31337, // Même chainId que le réseau hardhat
+      gas: "auto",
+      gasPrice: "auto",
+    },
+  
     
   },
 };
